13ca69d94cd793a56cbb599157b5bcdae2fe0077
toino <joaoapereira21@hotmail.com>
2022-08-26 15:07:38 +0100

    Add solutions to the day 11 puzzles

---
+++ day11/part1.py
@@ -0,0 +1,28 @@
+import itertools as it
+import more_itertools as mit
+import functools as ft
+import operator as op
+import re
+from collections import Counter
+
+
+def add_tuples(a, b):
+    return tuple(i + j for i, j in zip(a, b))
+
+
+with open("input") as inf, open("part1.out", "w+") as outf:
+    current = 0, 0, 0
+
+    dirs = {
+        "n": (0, -1, 1),
+        "ne": (1, -1, 0),
+        "se": (1, 0, -1),
+        "s": (0, 1, -1),
+        "sw": (-1, 1, 0),
+        "nw": (-1, 0, 1)
+    }
+
+    for d in inf.readline().strip().split(","):
+        current = add_tuples(current, dirs[d])
+
+    outf.write(str((abs(current[0]) + abs(current[1]) + abs(current[2])) // 2))
